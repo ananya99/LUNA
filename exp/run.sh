1937f8d0c4383fd2cb17a69864e7518f80813bca
ananya99 <ananya99gupta@gmail.com>
2025-04-25 21:15:27 +0000

    Add run.sh script

--- exp/run.sh
+++ exp/run.sh
@@ -5,7 +5,7 @@
 train_test_data_folder="train_test_split_1"
 
 root_directory="/home/anagupta/luna/$train_test_data_folder"
-output_path="$root_directory/output.txt"
+output_path="$root_directory/output.log"
 
 export HYDRA_FULL_ERROR=1 
-python3 /home/anagupta/luna/LUNA/main.py --config-path="$root_directory" --config-name=config.yaml > "$output_path"
+python3 /home/anagupta/luna/LUNA/main.py --config-path="$root_directory" --config-name=config.yaml | tee "$output_path"
